b07d289331f5c09f5bda7649763ec3e0095faec4
g0gab1s <83614025+GabrielaGodek@users.noreply.github.com>
2023-12-01 13:12:07 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
@@ -30,5 +31,5 @@ export default defineConfig({
       },
     },
   },
-  
+
 });
