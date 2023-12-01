77ee0d7a7b0415e921306f4b2b1e03c7ff2c455e
g0gab1s <83614025+GabrielaGodek@users.noreply.github.com>
2023-12-01 11:43:49 +0100

    optimize

--- src/components/FooterItem.vue
+++ src/components/FooterItem.vue
@@ -1,22 +1,36 @@
 <template>
   <div class="wrapper">
     <div class="contact_icon">
-      <a href="https://www.linkedin.com/in/gabrielagodek/">
-        <font-awesome-icon icon="fa-brands fa-linkedin" size="lg" aria-label="LinkedIn" style="color: #343837" />
+      <a href="https://www.linkedin.com/in/gabrielagodek/" aria-label="LinkedIn profile">
+        <font-awesome-icon icon="fa-brands fa-linkedin" size="lg" style="color: #343837" />
+        <span class="sr-only">LinkedIn</span>
       </a>
-      <a href="https://github.com/GabrielaGodek">
-        <font-awesome-icon icon="fa-brands fa-github" size="lg" aria-label="GitHub" style="color: #343837" />
+      <a href="https://github.com/GabrielaGodek" aria-label="GitHub profile">
+        <font-awesome-icon icon="fa-brands fa-github" size="lg" style="color: #343837" />
+        <span class="sr-only">GitHub</span>
       </a>
-      <a href="mailto:godekgabriela39@gmail.com">
-        <font-awesome-icon icon="fa-solid fa-envelope-open-text" size="lg" aria-label="mail" style="color: #343837" />
+      <a href="mailto:godekgabriela39@gmail.com" aria-label="Send an email">
+        <font-awesome-icon icon="fa-solid fa-envelope-open-text" size="lg" style="color: #343837" />
+        <span class="sr-only">Email</span>
       </a>
-      <a
-        href="ENG_GabrielaGodek.pdf"
-        download=""
-      >
-        <font-awesome-icon icon="fa-regular fa-file" size="lg" aria-label="resume" style="color: #343837" />
+      <a href="ENG_GabrielaGodek.pdf" download="" aria-label="Download resume">
+        <font-awesome-icon icon="fa-regular fa-file" size="lg" style="color: #343837" />
+        <span class="sr-only">Resume</span>
       </a>
     </div>
     <div class="signature">Gabriela <br class="d_hide" />Godek</div>
   </div>
 </template>
+
+<style scoped>
+.sr-only {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  padding: 0;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  border: 0;
+}
+</style>
