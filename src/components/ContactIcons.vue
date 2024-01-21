fa13910be904c002ebc91dbfc21cf914941915c8
g0gab1s <83614025+GabrielaGodek@users.noreply.github.com>
2024-01-21 19:46:22 +0100

    update animations

--- src/components/ContactIcons.vue
+++ src/components/ContactIcons.vue
@@ -14,22 +14,22 @@ export default defineComponent({
 <template>
   <div class="contact-icon">
     <a href="https://www.linkedin.com/in/gabrielagodek/" aria-label="LinkedIn profile" rel="noopener" class="contact-icon__link">
-      <font-awesome-icon icon="fa-brands fa-linkedin" size="lg" width="23" height="23" loading="lazy"
+      <font-awesome-icon icon="fa-brands fa-linkedin" size="lg" width="75" height="75" loading="lazy"
         :style="{ color: color }" />
       <span class="contact-icon__label">LinkedIn</span>
     </a>
     <a href="https://github.com/GabrielaGodek" aria-label="GitHub profile" rel="noopener" class="contact-icon__link">
-      <font-awesome-icon icon="fa-brands fa-github" size="lg" width="23" height="23" loading="lazy"
+      <font-awesome-icon icon="fa-brands fa-github" size="lg" width="75" height="75" loading="lazy"
         :style="{ color: color }" />
       <span class="contact-icon__label">GitHub</span>
     </a>
     <a href="mailto:godekgabriela39@gmail.com" aria-label="Send an email">
-      <font-awesome-icon icon="fa-solid fa-envelope-open-text" size="lg" width="23" height="23" loading="lazy" class="contact-icon__link"
+      <font-awesome-icon icon="fa-solid fa-envelope-open-text" size="lg" width="75" height="75" loading="lazy" class="contact-icon__link"
         :style="{ color: color }" />
       <span class="contact-icon__label">Email</span>
     </a>
     <a href="GabrielaGodek_ENG.pdf" download="" aria-label="Download resume" class="contact-icon__link">
-      <font-awesome-icon icon="fa-regular fa-file" size="lg" width="23" height="23" loading="lazy"
+      <font-awesome-icon icon="fa-regular fa-file" size="lg" width="75" height="75" loading="lazy"
         :style="{ color: color }" />
       <span class="contact-icon__label">Resume</span>
     </a>
@@ -39,6 +39,14 @@ export default defineComponent({
 <style scoped>
 .contact-icon {
   z-index: 99;
+  padding: 10px 0;
+  position: relative;
+}
+.contact-icon__link {
+  transition: transform .3s ease;
+}
+.contact-icon__link:hover {
+  transform: translateY(-5px);
 }
 
 .contact-icon__label{
