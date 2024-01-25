7f2b5475d3c0f353598fc70ebaf6be8576d8c2e9
g0gab1s <83614025+GabrielaGodek@users.noreply.github.com>
2024-01-25 15:37:33 +0100

    fix dep issue

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
-import ScrollParallax from 'vue3-parallax/src/components/ScrollParallax.vue';
-// import VueAnimateOnScroll from 'vue-animate-onscroll'
+
 import vueClickOutsideElement from 'vue-click-outside-element'
 import { MotionPlugin } from '@vueuse/motion'
 
@@ -12,8 +11,6 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelopeOpenText, faLaptopCode } from '@fortawesome/free-solid-svg-icons'
 import { faFile } from '@fortawesome/free-regular-svg-icons/faFile'
 
-// import AOS from 'aos'
-// import 'aos/dist/aos.css'
 
 import '@/scss/style.css'
 import nprogressBar from '@/includes/nprogress'
@@ -24,7 +21,6 @@ library.add(faGithub, faLinkedin, faEnvelopeOpenText, faFile, faLaptopCode)
 app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(router)
-app.component('scroll-parallax', ScrollParallax);
 nprogressBar(router)
 app.use(vueClickOutsideElement)
 // app.use(VueAnimateOnScroll)
